217158b1d325d684b0b0a1a4796c0e4ad924065b
Bhumika <dbhumika829@gmail.com>
2025-07-26 11:45:17 +0530

    Changing logo and Tagline

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -19,10 +19,10 @@ if(searchQuery.length > 0 ){
     },[searchQuery])
 
        return (
-        <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
+        <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-1 border-b border-gray-300 bg-white relative transition-all">
 
             <NavLink onClick={()=> setOpen(false)} to='/'>
-              <img  className=" h-9"src={assets.logo} alt="logo" />
+              <img  className=" h-14 md:h-20"src={assets.logo} alt="logo" />
             </NavLink>
 
             {/* Desktop Menu */}
@@ -70,7 +70,7 @@ if(searchQuery.length > 0 ){
                <NavLink to='/' onClick={()=> setOpen(false)}> Home</NavLink>
                 <NavLink to='/products' onClick={()=> setOpen(false)}> All Products</NavLink>
                 {user &&
-                <NavLink to='/' onClick={()=> setOpen(false)}> My Orders</NavLink>
+                <NavLink to='/my-orders' onClick={()=> setOpen(false)}> My Orders</NavLink>
 }
 <NavLink to='/contact' onClick={()=> setOpen(false)}> Contact</NavLink> 
 {!user ? (
